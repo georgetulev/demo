af6aa6bdd71c8212a2ac4bcd9413a5d5e03bf9d6
Tulev <georgetulev@abv.bg>
2022-08-22 11:11:25 +0300

    Adding route to handle/process conversation files

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
+use App\Services\ConversationProcessor;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/conversations/{conversationId}', function ($conversationId) {
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    return (new ConversationProcessor())->handle($conversationId);
+})->where('conversationId', '[0-9]+');
