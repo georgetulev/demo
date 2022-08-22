41b9e8d6cd582a294bbcd00170f0b702247bf002
Tulev <georgetulev@abv.bg>
2022-08-22 11:13:38 +0300

    Adding artisan command as an alternative for processing conversation files

---
+++ app/Console/Commands/ProcessConversationCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Services\ConversationProcessor;
+
+class ProcessConversationCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'process:conversation {conversation : The ID of the conversation.}';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Processing conversation files. {conversation : The ID of the conversation.}';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        try {
+            $result = (new ConversationProcessor())->handle($this->argument('conversation'));
+        } catch (\Throwable $exception) {
+            $this->info("Conversation processing failed for ID: {$this->argument('conversation')}!"
+                . ' Reason: ' . $exception->getMessage());
+
+            return 0;
+        }
+
+        $this->info(json_encode($result));
+
+        return 1;
+    }
+}
